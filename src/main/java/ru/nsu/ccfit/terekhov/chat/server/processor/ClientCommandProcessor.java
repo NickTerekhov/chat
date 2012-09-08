d7c5aaa2aabe5c61dde9334f7298128cd2faf4d8
lewbor <boris.alperin@gmail.com>
2012-09-08 09:01:59 +0700

    Fixed problem with null check

--- src/main/java/ru/nsu/ccfit/terekhov/chat/server/processor/ClientCommandProcessor.java
+++ src/main/java/ru/nsu/ccfit/terekhov/chat/server/processor/ClientCommandProcessor.java
@@ -6,44 +6,42 @@ import ru.nsu.ccfit.terekhov.chat.server.transfer.impl.xml.XmlClientSocketProces
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-public class ClientCommandProcessor implements Runnable
-{
-	private final static int QUEUE_SIZE = 100;
-	private final static long DELAY_TIME = 1000;
-	private final ArrayBlockingQueue<CommandTask> commandTasksQueue = new ArrayBlockingQueue<CommandTask>(QUEUE_SIZE);
+public class ClientCommandProcessor implements Runnable {
+    private final static int QUEUE_SIZE = 100;
+    private final static long DELAY_TIME = 1000;
+    private final ArrayBlockingQueue<CommandTask> commandTasksQueue = new ArrayBlockingQueue<CommandTask>(QUEUE_SIZE);
 
-	public void addCommandTask(CommandTask commandTask) throws InterruptedException
-	{
-		assert null != commandTask;
-		commandTasksQueue.put(commandTask);
-	}
+    public void addCommandTask(CommandTask commandTask) throws InterruptedException {
+        assert null != commandTask;
+        commandTasksQueue.put(commandTask);
+    }
 
 
-	@Override
-	public void run()
-	{
-		for (; ; ) {
-			try {
-				CommandTask task = commandTasksQueue.poll(DELAY_TIME, TimeUnit.MICROSECONDS);
-				if (null == task && Thread.currentThread().isInterrupted()) {
-					// todo replace with logger
-					System.out.println("Finishing commandprocessor thread");
-					return;
-				}
-				processTask(task);
+    @Override
+    public void run() {
+        for (; ; ) {
+            try {
+                CommandTask task = commandTasksQueue.poll(DELAY_TIME, TimeUnit.MICROSECONDS);
+                if (null == task && Thread.currentThread().isInterrupted()) {
+                    // todo replace with logger
+                    System.out.println("Finishing commandprocessor thread");
+                    return;
+                }
+                if (null != task) {
+                    processTask(task);
+                }
 
-			} catch (InterruptedException e) {
-				// todo replace with logger
-				e.printStackTrace();
-			}
+            } catch (InterruptedException e) {
+                // todo replace with logger
+                e.printStackTrace();
+            }
 
-		}
-	}
+        }
+    }
 
-	private void processTask(CommandTask task)
-	{
-
-	}
+    private void processTask(CommandTask task) {
+        assert null != task;
+    }
 
 
 }
